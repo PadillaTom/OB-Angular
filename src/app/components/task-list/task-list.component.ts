07685d3c3d21a95152257a29532f1ae3b9a05b45
Tomas Padilla <padillatomasagustin@gmail.com>
2023-04-20 12:18:05 +0200

    final-poject - Display TASK in TASKLIST

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ITask, Levels } from 'src/app/models/interfaces/Task.interface';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.scss'],
 })
 export class TaskListComponent {
-
+  task1: ITask = {
+    title: 'Task 1',
+    description: 'Description 1',
+    completed: false,
+    level: Levels.Info,
+  };
+  task2: ITask = {
+    title: 'Task 2',
+    description: 'Description 2',
+    completed: true,
+    level: Levels.Urgent,
+  };
 }
--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ITask } from 'src/app/models/interfaces/Task.interface';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.scss']
+  styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent {
+  @Input() task: ITask | undefined;
 
+  handleDelete() {
+    console.log('[TASK] handleDelete(): ' + this.task?.title);
+  }
 }
